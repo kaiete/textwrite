f9d151210374413d34880631e69a25c6b2f4c74c
kaiete <kaiete@aragon-press.com>
2021-02-06 17:09:48 +0000

    Update textwrite.py

--- textwrite.py
+++ textwrite.py
@@ -1,6 +1,3 @@
 import time
 print("TextWrite, the free, open-source text editor. Reimagined, in Python.")
-time.sleep(1000)
-print("I have nothing to say, so this window will now close.")
-time.sleep(500)
 quit()
