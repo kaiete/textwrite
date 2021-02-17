e37f75186b3d8cdc186dfb4837a563c80c77b170
kaiete <kaiete@aragon-press.com>
2021-02-17 09:52:59 +0000

    Update tw.js

--- tw.js
+++ tw.js
@@ -6,13 +6,21 @@ var textwrite = {
 
 const hostname = '127.0.0.1'
 const port = portToHost;
+    
+    function addToFile() {
+      var text_to_add = prompt("Please enter some text to add to the file (it will appear on a new line)")
+      fs.appendFile('textwrite.tw', "<br>" + text_to_add, function (err) {
+  if (err) throw err;
+  console.log('Saved!');
+});
+    }
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   res.write('<h1 style="font-family: Courier;">TextWrite server</h1>');
   res.write(`<head><title>TextWrite</title></head>`)
-  res.write(`<p style="font-family: Courier;">TextWrite servers don't do much yet, you can only view files.<br>The file has to be in the same place as the server, and must be <b>textwrite.tw</b>.</p>`)
+  res.write(`<p style="font-family: Courier;">TextWrite servers don't do much yet, you can only view files.<br>The file has to be in the same place as the server, and must be <b>textwrite.tw</b>.</p><br><button onclick='addToFile()'>Add to file textwrite.tw</button>`)
     fs.readFile('textwrite.tw', function(err, data) {
     res.write('<p>' + data + '</p>');
     return res.end();
