1e34a8c0401661ecad6522cfec282b416c4f9bed
kaiete <kaiete@aragon-press.com>
2021-02-25 14:06:49 +0000

    finally, textwrite in working python

--- tw.py
+++ tw.py
@@ -1,5 +1,18 @@
-print("Appname:")
+print("App name:")
 appname = "textwrite"
 print(appname)
-print("Version can be found at https://github.com/kaiete/textwrite/blob/main/version.txt")
+print("Getting version - please wait...")
+version = open("version.txt")
+if version != "1.0.0":
+  print("Version supported, please enter name of file to open or help for help")
+  filename = input("Filename: ")
+  if filename == "help":
+    print("Filename can be either /example.txt, which would look for example.txt in root directory, or example.txt, which will look for example.txt in this directory.")
+  elif filename == "":
+    print("Filename can be either /example.txt, which would look for example.txt in root directory, or example.txt, which will look for example.txt in this directory.")
+  else:
+    filecontents = open(filename)
+else:
+  print("Version not supported, please upgrade.")
+    
 
