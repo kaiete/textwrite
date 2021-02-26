7f657d190f8750f51f5ab9641707b3285ccd8552
kaiete <kaiete@aragon-press.com>
2021-02-26 09:58:28 +0000

    kaiete says: "hello"

--- tw.py
+++ tw.py
@@ -5,14 +5,8 @@ print("Getting version - please wait...")
 version = open("version.txt")
 if version != "1.0.0":
   print("Version supported, please enter name of file to open or help for help")
-  filename = input("Filename: ")
-  if filename == "help":
-    print("Filename can be either /example.txt, which would look for example.txt in root directory, or example.txt, which will look for example.txt in this directory.")
-  elif filename == "":
-    print("Filename can be either /example.txt, which would look for example.txt in root directory, or example.txt, which will look for example.txt in this directory.")
-  else:
-    filecontents = open(filename)
+  filename = input("Filename wrapped in quotes: ")
+  filecontents = open(filename)
+  print(filecontents.read())
 else:
   print("Version not supported, please upgrade.")
-    
-
